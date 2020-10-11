275ccc1f579b002a96cf04e8b35bfe7be80e0a24
StarDragoYT <69414355+StarDragoYT@users.noreply.github.com>
2020-10-11 12:27:31 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,18 @@
+var timerId;
+
+function startTimer() {
+  time = 5;
+  timer.innerHTML = time;
+  timerId = setInterval(() => {
+    time--;
+    if (time == 0) location.href = "./gameover.html?score=" + score;
+    timer.innerHTML = time;
+  }, 1000);
+}
+
+function resetTime(intervalId) {
+  clearInterval(intervalId);
+  startTimer();
+}
+
+startTimer();
